eea446680f5b4a942c5904f080579f48ff10bfa4
Mykhailo Myshko <m.myshko@uds.systems>
2026-05-16 23:41:56 +0300

    1.0.3

--- app/threem.HA.Addon.DataProvider/threem.HA.Addon.DataProvider/Program.cs
+++ app/threem.HA.Addon.DataProvider/threem.HA.Addon.DataProvider/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
