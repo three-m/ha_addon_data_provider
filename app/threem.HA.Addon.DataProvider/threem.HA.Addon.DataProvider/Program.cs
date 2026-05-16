5e200e138c1c04e6d32c945be7c7bd03493ac559
Mykhailo Myshko <m.myshko@uds.systems>
2026-05-16 23:11:06 +0300

    fix ha ingress path

--- app/threem.HA.Addon.DataProvider/threem.HA.Addon.DataProvider/Program.cs
+++ app/threem.HA.Addon.DataProvider/threem.HA.Addon.DataProvider/Program.cs
@@ -6,17 +6,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Headers.TryGetValue("X-Ingress-Path", out var ingressPath))
+    {
+        var path = ingressPath.ToString();
+        if (!path.EndsWith("/")) path += "/";
+        context.Items["IngressBasePath"] = path;
+    }
+    await next();
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
@@ -24,4 +40,4 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-app.Run("http://0.0.0.0:8080");
+app.Run();
